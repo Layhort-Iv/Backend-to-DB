806040d0a17349490e159fbba61ad5cedc23d763
Iv Layhort <ivlayhort18@kit.edu.kh>
2020-09-28 23:47:43 +0700

    Handle spa

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ const tasklist = require("../api/tasklist");
 
 app.use("/api/tasklist", tasklist);
 
+//handle prouction
+if (process.env.NODE_ENV === "production") {
+  //static fider
+  app.use(express.static(__dirname + "/public"));
+
+  //handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "public/index.html"));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
